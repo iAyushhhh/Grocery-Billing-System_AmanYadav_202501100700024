37c50cbf36cd27c577dca90880eedb5ffdd88628
Ayush Yadav <yadavayush181028@gmail.com>
2026-02-20 21:37:19 +0530

    Update Hehe.py

--- Hehe.py
+++ Hehe.py
@@ -1,11 +1,15 @@
-#Grocery Billing System
+print("Welcome to the store..........")
+print()
 a=int(input("Enter price of item 1: "))
 b=int(input("Enter price of item 2: "))
 c=int(input("Enter price of item 3: "))
 total=a+b+c
 discount=0
-print("Total price: ",total)
+print()
+print("\t\tTotal price: ",total)
 if total>50:
    discount=total*0.1
-   print("Discount applied: ",discount)
-print("Final amount: ",total-discount)
+   print(f"\t\tDiscount applied: {discount:.2f}")
+print(f"\t\tFinal amount: {total-discount:.2f}")
+print("\n")
+print("\tThank you for shopping with us.......")
